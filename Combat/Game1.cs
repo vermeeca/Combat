107e8d89a2126ce25157e51b2c33c12f0902ac78
Craig Vermeer <craig@vermeers.net>
2010-05-17 12:57:06 -0400

    Starting with basically a blank XNA app this time

---
+++ Combat/Game1.cs
@@ -0,0 +1,228 @@
+﻿#region File Description
+//-----------------------------------------------------------------------------
+// Game1.cs
+//
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Surface.Core;
+
+namespace Combat
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private Vector2 ballPosition;
+
+        private ContactTarget contactTarget;
+
+
+        /// <summary>
+        /// The target receiving all surface input for the application.
+        /// </summary>
+        protected ContactTarget ContactTarget
+        {
+            get { return contactTarget; }
+        }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            SetWindowOnSurface();
+            InitializeSurfaceInput();
+
+            spritePosition = new Vector2(graphics.GraphicsDevice.Viewport.Width - 100, graphics.GraphicsDevice.Viewport.Height / 2);
+
+            base.Initialize();
+        }
+        /// <summary>
+        /// Moves and sizes the window to cover the input surface.
+        /// </summary>
+        private void SetWindowOnSurface()
+        {
+            System.Diagnostics.Debug.Assert(Window.Handle != System.IntPtr.Zero,
+                "Window initialization must be complete before SetWindowOnSurface is called");
+            if (Window.Handle == System.IntPtr.Zero)
+                return;
+
+            // We don't want to run in full-screen mode because we need
+            // overlapped windows, so instead run in windowed mode
+            // and resize to take up the whole surface with no border.
+
+            // Make sure the graphics device has the correct back buffer size.
+            InteractiveSurface interactiveSurface = InteractiveSurface.DefaultInteractiveSurface;
+            if (interactiveSurface != null)
+            {
+                graphics.PreferredBackBufferWidth = interactiveSurface.Width;
+                graphics.PreferredBackBufferHeight = interactiveSurface.Height;
+                graphics.ApplyChanges();
+
+                // Remove the border and position the window.
+                Program.RemoveBorder(Window.Handle);
+                Program.PositionWindow(Window);
+            }
+        }
+
+        /// <summary>
+        /// Initializes the surface input system. This should be called after any window
+        /// initialization is done, and should only be called once.
+        /// </summary>
+        private void InitializeSurfaceInput()
+        {
+            System.Diagnostics.Debug.Assert(Window.Handle != System.IntPtr.Zero,
+                "Window initialization must be complete before InitializeSurfaceInput is called");
+            if (Window.Handle == System.IntPtr.Zero)
+                return;
+            System.Diagnostics.Debug.Assert(contactTarget == null,
+                "Surface input already initialized");
+            if (contactTarget != null)
+                return;
+
+            // Create a target for surface input.
+            contactTarget = new ContactTarget(Window.Handle, EventThreadChoice.OnBackgroundThread);
+            contactTarget.EnableInput();
+        }
+
+
+        // This is a texture we can render.
+        Texture2D myTexture;
+
+        // Set the coordinates to draw the sprite at.
+        Vector2 spritePosition = Vector2.Zero;
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            myTexture = Content.Load<Texture2D>("Tank");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        // Store some information about the sprite's motion.
+        Vector2 spriteSpeed = new Vector2(100.0f, 100.0f);
+        private KeyboardState keyState;
+        private float rotation = 0;
+
+        private static readonly float CIRCLE = MathHelper.Pi * 2;
+
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == 
+                ButtonState.Pressed)
+                this.Exit();
+
+            keyState = Keyboard.GetState();
+
+            // Move the sprite around.
+            UpdateSprite(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        void UpdateSprite(GameTime gameTime) 
+        {
+            UpdateRotation();
+            // Move the sprite by speed, scaled by elapsed time.
+            //spritePosition += 
+                //spriteSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            int MaxX = 
+                graphics.GraphicsDevice.Viewport.Width - myTexture.Width;
+            int MinX = 0;
+            int MaxY = 
+                graphics.GraphicsDevice.Viewport.Height - myTexture.Height;
+            int MinY = 0;
+
+            // Check for bounce.
+            if (spritePosition.X > MaxX)
+            {
+                spriteSpeed.X *= -1;
+                spritePosition.X = MaxX;
+            }
+
+            else if (spritePosition.X < MinX)
+            {
+                spriteSpeed.X *= -1;
+                spritePosition.X = MinX;
+            }
+
+            if (spritePosition.Y > MaxY)
+            {
+                spriteSpeed.Y *= -1;
+                spritePosition.Y = MaxY;
+            }
+
+            else if (spritePosition.Y < MinY)
+            {
+                spriteSpeed.Y *= -1;
+                spritePosition.Y = MinY;
+            }
+        }
+
+        private void UpdateRotation()
+        {
+            if (keyState.IsKeyDown(Keys.Left))
+            {
+                rotation -= .035f;
+            }
+            if (keyState.IsKeyDown(Keys.Right))
+            {
+                rotation += .035f;
+            }
+        }
+
+
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Draw the sprite.
+            spriteBatch.Begin(SpriteBlendMode.AlphaBlend);
+
+            Vector2 origin = new Vector2(myTexture.Width / 2, myTexture.Height / 2);
+
+            spriteBatch.Draw(myTexture, spritePosition, null, Color.White, rotation % CIRCLE, origin, 1.0f, SpriteEffects.None, 0f);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Combat/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SurfaceApplication1")]
+[assembly: AssemblyProduct("SurfaceApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("BMW Corp")]
+
+[assembly: AssemblyCopyright("Copyright © BMW Corp 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c9cad819-77e2-4cf7-8535-204b003da6a7")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
